1f05d006a67570549cdacec97cc5724de8efb5b4
Fekleite <fernandacarolineleite360@gmail.com>
2021-04-05 22:34:12 -0300

    feat: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Center, Image } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Center w='100vw' h='100px' >
+      <Image src='/images/logo.svg' alt='World Trip'/>
+    </Center>
+  )
+}
